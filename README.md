93f3ac30a7a7dcd4a58bc9aeaf999aaa9b2575a8
zwh <1209681393@qq.com>
2017-05-04 17:08:31 +0800

    modify readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # crawler
 Using Python to crawl the popular website
+
+### Request
+> pandas  
+> selenium  
+> phantomjs  
+> BeautifulSoup
+
+### Prepare
+> pip install pandas  
+> pip install selenium  
+> set Phantomjs path in process_website.py
+> pip install bs4
+
+### Features
+- Crawl news website (http://news.qq.com)
+
+### Run
+```shell-script
+    python main.py
+```
+### Result
+The result can be found in data/result.csv. The csv contains title, body, time, comments.
--- src/process_website.py
+++ src/process_website.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # coding=utf-8
-from selenium import webdriver
+import random
+
 from bs4 import BeautifulSoup
+from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import re,random
 
 USER_AGENTS = ['Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
                'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A']
+
 phantomjs_path = '/home/zwh/software/phantomjs-2.1.1-linux-x86_64/bin/phantomjs'
 class website(object):
     
